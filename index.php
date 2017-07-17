803eeaeb97a35655861d37eb5eb8a4f04123ea59
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-07-18 08:06:37 +1000

    add description and keywords

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>OmniRate</title>
+        <title>OmniRate | Energy rating professionals in Cairns</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="description" content="We are Energy Rating Professionals in 
+              Cairns Queensland. Call us for any help with your NatHERS or 
+              Section J reports.">
+        <meta name="keywords" content="Sustainability, Solutions,
+              Energy Efficient, Energy, Ratings, efficiency, assessments,
+              residential, commercial, NatHERS, ABSA, BDAV, Star Rating, 6 Stars,
+              OmniRate, Omni, Rate, Professionals">
+
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="css/carousel.css" rel="stylesheet" type="text/css"/>
-        
+
         <?php include_once("analyticstracking.php") ?>
 
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-        
+
         <script src="js/holder.min.js" type="text/javascript"></script>
     </head>
     <body>
