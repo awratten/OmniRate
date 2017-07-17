a156217e87b86abd4d6aaa03b25b3f4863d06751
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-07-18 07:41:54 +1000

    update

--- index.php
+++ index.php
@@ -1,11 +1,219 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>OmniRate </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <div>TODO write content</div>
+
+
+
+
+        <div class="client-document-container">
+            <div id="client-document" class="address-on-left ">
+                <div id="client-document-status" class="client-doc-header">
+                    <div class="client-doc-name">
+                        <img alt="Logo for OmniRate" src="https://proxy.harvestfiles.com/production_harvestapp_public/uploads/account_logos/0027/5268/31ba1068955c2c3f5d5daa3cabc87eaa0d2c9676/thumbnail.png">
+                    </div>
+                    <div class="client-doc-doc-type">Fee Proposal</div>
+                    <div style="clear:right;"></div>
+                    <div class="client-doc-from client-doc-address">
+                        <h3>From</h3>
+                        <div>
+                            <strong>OmniRate</strong>
+                            <span class="company-address">
+                                ABN: 93081682473
+                                <br>
+                                Phone: 0438 045 878
+                                <br>
+                                awratten1@gmail.com
+                                <br>
+                                44 Bi-Centennial Road
+                                <br>
+                                Bentley Park - QLD - 4869<br>
+                            </span>
+                        </div>
+                    </div>
+                    <div style="clear:both;"></div>
+
+                    <div class="client-doc-for client-doc-address">
+                        <h3>Client</h3>
+                        <div>
+                            <strong>Elston Design</strong>
+                            <span class="company-address">
+                                E: garry@elstondesign.com.au
+                                <br>
+                                P: 0408 754 997<br>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="client-doc-details">
+                        <table cellspacing="0" cellpadding="0" border="0">
+                            <tbody>
+                                <tr>
+                                    <td class="label">Reference</td>
+                                    <td class="definition"><strong>1</strong></td>
+                                </tr>
+                                <tr class="purchase-order">
+                                    <td class="label">PO Number</td>
+                                    <td class="definition">E-E000280-IB</td>
+                                </tr>
+                                <tr>
+                                    <td class="label">Issue Date</td>
+                                    <td class="definition">04/07/2017</td>
+                                </tr>
+                                <tr class="subject subject-address-on-right">
+                                    <td class="label">Subject</td>
+                                    <td class="definition">Single Dwelling Lot 240 Annan Bend, Mount Peter QLD 4869 - 6 Star Energy Efficiency Assessment</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div style="clear:both;"></div>
+                    <div class="subject-address-on-left client-doc-address" style="display:none">
+                        <h3>Subject</h3>
+                        <div>Single Dwelling Lot 240 Annan Bend, Mount Peter QLD 4869 - 6 Star Energy Efficiency Assessment</div>
+                    </div>
+                </div>
+                <table class="client-doc-items" cellspacing="0" cellpadding="0" border="0">
+                    <thead class="client-doc-items-header desktop-only">
+                        <tr>
+                            <th class="item-type">Item Type</th>
+                            <th class="item-description">Description</th>
+                            <th class="item-qty">Quantity</th>
+                            <th class="item-unit-price">Unit Price</th>
+                            <th class="item-amount">Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody class="client-doc-rows">
+                        <tr>
+                            <td class="item-type desktop-only">Service</td>
+                            <td class="item-description">
+                                Thermal Comfort Software Simulation
+                                <br>
+                                NatHERS Assessment Using FirstRate 5
+                                <br>
+
+                                <br>
+                                Service Level Target (SLT):
+                                <br>
+                                We aim to achieve a target score of 6 Stars for your dwelling.
+
+                                <table class="item-details mobile-only">
+                                    <thead class="item-details-header">
+                                        <tr><th class="item-type">Item Type</th>
+                                            <th class="item-qty">Quantity</th>
+                                            <th class="item-unit-price">Unit Price</th>
+                                        </tr></thead>
+                                    <tbody><tr>
+                                            <td class="item-type">Service</td>
+                                            <td class="item-qty">1.00</td>
+                                            <td class="item-unit-price">$280.00</td>
+                                        </tr>
+                                    </tbody></table>
+                            </td>
+                            <td class="item-qty desktop-only">1.00</td>
+                            <td class="item-unit-price desktop-only">$280.00</td>
+                            <td class="item-amount">
+                                $280.00
+
+                                <span class="tax-column-span">
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="item-type desktop-only">Disbursement</td>
+                            <td class="item-description">
+                                Universal Certificate Fees
+
+                                <table class="item-details mobile-only">
+                                    <thead class="item-details-header">
+                                        <tr><th class="item-type">Item Type</th>
+                                            <th class="item-qty">Quantity</th>
+                                            <th class="item-unit-price">Unit Price</th>
+                                        </tr></thead>
+                                    <tbody><tr>
+                                            <td class="item-type">Disbursement</td>
+                                            <td class="item-qty">1.00</td>
+                                            <td class="item-unit-price">$20.00</td>
+                                        </tr>
+                                    </tbody></table>
+                            </td>
+                            <td class="item-qty desktop-only">1.00</td>
+                            <td class="item-unit-price desktop-only">$20.00</td>
+                            <td class="item-amount">
+                                $20.00
+
+                                <span class="tax-column-span">
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody class="client-doc-summary">
+                        <tr>
+                            <td class="label" colspan="4">Subtotal</td>
+                            <td class="subtotal">$300.00</td>
+                        </tr>
+                        <tr>
+                            <td class="label first" colspan="4">
+                                <span>
+                                    GST
+                                    <span class="tax-percent">(10%)</span>
+                                </span>
+                            </td>
+                            <td class="subtotal">$30.00</td>
+                        </tr>
+                        <tr class="total">
+                            <td class="label" colspan="4">Fee Proposal Total</td>
+                            <td id="total-amount" class="total">$330.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div class="client-doc-notes">
+                    <h3>Notes</h3>
+                    <div class="notes">
+                        Payment Terms:
+                        <br>
+                        - Our account is to be settled in full prior to the release of final reports and certification.
+                        <br>
+                        - The quote is valid for 10 days from the date of issue. If the quote is accepted after this period; quoted fees may vary.
+                        <br>
+                        - This fee proposal is strictly based on the information provided to our company at the time of the quote being prepared.
+                        <br>
+                        Should further and additional information come to light once work has commenced, our company reserves the right to cease to proceed with the work unless the quote is varied and the customer agrees to the varied quote.
+                        <br>
+                        - If additional optimisation time is required, we will notify you and you can decide if you wish to proceed at our reduced hourly rates of $50/h.
+                        <br>
+                        - Once the Customer's final drawings have been stamped and issued, additional fees may apply for re-stamping and certifying the project.
+                        <br>
+
+                        <br>
+                        Completion Timeframe: Depending on the scale of the project, we are generally able to work to your project delivery timeframe commitments.
+                        <br>
+
+                        <br>
+                        Anthony Wratten
+                        <br>
+                        Cert IV (NatHERS Assessment)
+                        <br>
+
+                        <br>
+                        ABSA Assessor 
+                        <br>
+                        Director
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+
+            </div>
+
+            <div id="file_attachments" class="no_files">
+
+            </div>
+
+        </div>
     </body>
 </html>
